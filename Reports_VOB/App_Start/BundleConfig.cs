8858c89ce4711dedf46011c129abd395e6a852dd
Jose Yañez <josey@iconexa.com>
2016-09-06 18:25:07 -0300

    Initial Release

---
+++ Reports_VOB/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace VOB.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Usamos la version de Jqueryu que utiliza KendoUI para evitar conflictos.
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/kendo/2016.2.714/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate.js"
+                        , "~/Scripts/jquery.validate.unobtrusive.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.6.2.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/site.css"));
+
+            /* KENDO UI */
+            bundles.Add(new StyleBundle("~/Content/KendoCss").Include(
+                "~/Content/Kendo/2016.2.714/kendo.common.min.css",
+                "~/Content/Kendo/2016.2.714/kendo.default.min.css",
+                "~/Content/Kendo/2016.2.714/kendo.default.mobile.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/KendoJs").Include(
+                "~/Scripts/kendo/2016.2.714/kendo.all.min.js"));
+        }
+    }
+}
