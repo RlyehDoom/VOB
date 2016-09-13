5491a526754cea4ee328f5b9c2d7c41cbc0332a9
Jose Yañez <josey@iconexa.com>
2016-09-13 12:25:31 -0300

    Gradient changes

---
+++ Reports_VOB/Controllers/ActividadController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VOB.Web.Controllers
+{
+    public class ActividadController : Utilidades.BaseController
+    {
+        public ActionResult Index(Models.ReporteNormalModel model)
+        {
+            model.IdTipoBalance = 4;
+            model.TituloBalance = "Conceptual";
+            ViewBag.Title = "Banco Internacional - Reporte Financiero Conceptual";
+            ViewBag.UserName = UsuarioNT;
+
+            return View("Index", model);
+        }
+    }
+}
